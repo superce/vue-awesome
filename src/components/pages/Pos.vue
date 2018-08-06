dbb5f068d03a8218d5ff9f2f350fbea81d87c8be
superce <562923778@qq.com>
2018-08-06 23:52:48 +0800

    删除axios

--- src/components/pages/Pos.vue
+++ src/components/pages/Pos.vue
@@ -104,36 +104,36 @@
 </template>
 
 <script>
-  import axios from 'axios'
+  // import axios from 'axios'
   export default {
     name: 'Pos',
     data: () => ({
       tableData: [],
-      oftenGoods:[],
-      type0Goods:[],
-      type1Goods:[],
-      type2Goods:[],
-      type3Goods:[],
+      oftenGoods:[ { "goodsId":1, "goodsName":"香辣鸡腿堡", "price":18 }, { "goodsId":2, "goodsName":"田园鸡腿堡", "price":15 }, { "goodsId":3, "goodsName":"和风汉堡", "price":15 }, { "goodsId":4, "goodsName":"大包薯条", "price":18 }, { "goodsId":5, "goodsName":"脆皮炸鸡腿", "price":20 }, { "goodsId":6, "goodsName":"魔法鸡块", "price":20 }, { "goodsId":7, "goodsName":"可乐大杯", "price":10 }, { "goodsId":8, "goodsName":"雪顶咖啡", "price":18 }, { "goodsId":9, "goodsName":"儿童欢乐套餐", "price":25 }, { "goodsId":10, "goodsName":"快乐全家桶", "price":99 } ],
+      type0Goods: [ { "goodsId": 1, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg", "goodsName": "香辣鸡腿堡", "price": 18 }, { "goodsId": 2, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg", "goodsName": "田园鸡腿堡", "price": 15 }, { "goodsId": 3, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg", "goodsName": "和风汉堡", "price": 15 } ],
+      type1Goods: [ { "goodsId": 4, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg", "goodsName": "大包薯条", "price": 18 }, { "goodsId": 5, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg", "goodsName": "脆皮炸鸡腿", "price": 20 }, { "goodsId": 6, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg", "goodsName": "魔法鸡块", "price": 20 } ],
+      type2Goods:[ { "goodsId": 7, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg", "goodsName": "可乐大杯", "price": 10 }, { "goodsId": 8, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg", "goodsName": "雪顶咖啡", "price": 18 } ],
+      type3Goods:[ { "goodsId": 9, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg", "goodsName": "儿童欢乐套餐", "price": 25 }, { "goodsId": 10, "goodsImg": "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg", "goodsName": "快乐全家桶", "price": 99 } ] ,
       totalCount:0,
       totalMoney:0
     }),
-    created(){
-      // 读取常用商品
-      let url = 'http://jspang.com/DemoApi/oftenGoods.php';
-      axios.get(url)
-      .then(res => this.oftenGoods = res.data)
-      .catch(e => alert('网络错误'))
-      // 读取分类
-      axios.get('http://jspang.com/DemoApi/typeGoods.php')
-      .then(res => {
-        this.type0Goods = res.data[0]
-        this.type1Goods = res.data[1]
-        this.type2Goods = res.data[2]
-        this.type3Goods = res.data[3]
-        }
-      )
-      .catch(e => console.log(e))
-    },
+    // created(){
+    //   // 读取常用商品
+    //   let url = 'https://jspang.com/DemoApi/oftenGoods.php';
+    //   axios.get(url)
+    //   .then(res => this.oftenGoods = res.data)
+    //   .catch(e => alert('网络错误'))
+    //   // 读取分类
+    //   axios.get('http://jspang.com/DemoApi/typeGoods.php')
+    //   .then(res => {
+    //     this.type0Goods = res.data[0]
+    //     this.type1Goods = res.data[1]
+    //     this.type2Goods = res.data[2]
+    //     this.type3Goods = res.data[3]
+    //     }
+    //   )
+    //   .catch(e => console.log(e))
+    // },
     methods:{
       addOrder(goods){       
         // 判断点餐列表是否有订单
